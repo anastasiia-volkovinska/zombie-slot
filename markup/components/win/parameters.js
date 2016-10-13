4f445d5592a0f97b50cd1d3a626112e79c012354
Nastya <nastya.volkovinskaya@gmail.com>
2016-10-13 18:30:04 +0300

    line animation and splash close #16 #15

--- markup/components/win/parameters.js
+++ markup/components/win/parameters.js
@@ -2,53 +2,103 @@ export const parameters = {
     font: 'normal 15px Arial',
     color: 'gold',
     1: {
-        x: 91,
-        y: 366,
+        x: 35,
+        y: 316,
         textBaseline: 'middle'
     },
     2: {
-        x: 91,
-        y: 149,
+        x: 40,
+        y: 115,
         textBaseline: 'middle'
     },
     3: {
-        x: 91,
-        y: 527,
+        x: 40,
+        y: 467,
         textBaseline: 'middle'
     },
     4: {
-        x: 91,
-        y: 96,
+        x: 45,
+        y: 66,
         textBaseline: 'middle'
     },
     5: {
-        x: 91,
-        y: 581,
+        x: 40,
+        y: 516,
         textBaseline: 'middle'
     },
     6: {
-        x: 91,
-        y: 201,
+        x: 35,
+        y: 161,
         textBaseline: 'middle'
     },
     7: {
-        x: 91,
-        y: 473,
+        x: 40,
+        y: 413,
         textBaseline: 'middle'
     },
     8: {
-        x: 91,
-        y: 419,
+        x: 35,
+        y: 369,
         textBaseline: 'middle'
     },
     9: {
-        x: 91,
-        y: 256,
+        x: 35,
+        y: 216,
         textBaseline: 'middle'
     },
     10: {
-        x: 91,
-        y: 311,
+        x: 35,
+        y: 265,
+        textBaseline: 'middle'
+    },
+    11: {
+        x: 1035,
+        y: 316,
+        textBaseline: 'middle'
+    },
+    12: {
+        x: 1020,
+        y: 115,
+        textBaseline: 'middle'
+    },
+    13: {
+        x: 1020,
+        y: 467,
+        textBaseline: 'middle'
+    },
+    14: {
+        x: 1020,
+        y: 66,
+        textBaseline: 'middle'
+    },
+    15: {
+        x: 1020,
+        y: 516,
+        textBaseline: 'middle'
+    },
+    16: {
+        x: 1020,
+        y: 161,
+        textBaseline: 'middle'
+    },
+    17: {
+        x: 1025,
+        y: 413,
+        textBaseline: 'middle'
+    },
+    18: {
+        x: 1030,
+        y: 369,
+        textBaseline: 'middle'
+    },
+    19: {
+        x: 1025,
+        y: 216,
+        textBaseline: 'middle'
+    },
+    110: {
+        x: 1030,
+        y: 265,
         textBaseline: 'middle'
     }
 };
